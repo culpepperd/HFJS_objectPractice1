08d6011383c4a2d4ac8b2002405832a93f3897f5
culpepperd <culpepperd2490@gmail.com>
2023-06-09 01:31:21 -0400

    added fuel method to Chevy and while loop to test postdecrementing fuel after call drive() method

--- objectPractice1.js
+++ objectPractice1.js
@@ -106,24 +106,35 @@ var chevy = {
 	convertible: false,
 	mileage: 1021,
 	started: false,
+	fuel: 0,
 
 	start: function () {
-		this.started = true;
-		console.log("You started the " + this.model + " engine.");
+		if (this.fuel > 0) {
+			this.started = true;
+			console.log("You started the " + this.model + " engine, and you have " + this.fuel + " gallons of fuel.");
+		} else {
+			console.log("You need to add fuel.");
+        }
+
 	},
 
 	stop: function () {
 		this.started = false;
-		console.log("You stopped the " + this.model + " engine.");
+		console.log("The " + this.model + " engine is stopped.");
 	},
 
 	drive: function () {
-		console.log("You're attempting to drive the " + this.make + " " + this.model + ".");
+		// console.log("You're attempting to drive the " + this.make + " " + this.model + ".");
 		if (this.started) {
 			alert(this.make + " " + this.model + " goes zoom zoom!");
+			this.fuel = this.fuel - 1;
 		} else {
 			alert("You need to start the engine first.");
 		}
+	},
+
+	addFuel: function (amount) {
+		this.fuel = this.fuel + amount;
 	}
 };
 
@@ -137,12 +148,28 @@ function prequal(car) {
 	return true;
 }
 
-fiat.addFuel(5);
-console.log("Fuel level in " + fiat.make + " " + fiat.model + " is: " + fiat.fuel + " gallons.");
+chevy.addFuel(5);
+
+while (chevy.fuel > 0) {
+	console.log("Fuel level in " + chevy.make + " " + chevy.model + " is: " + chevy.fuel + " gallons.");
+	console.log("Let's take her for a spin!");
+	if (chevy.started) {
+		chevy.drive();
+	} else {
+		console.log("You need to start the car first...");
+		chevy.start();
+	}
+
+	if (chevy.fuel == 0) {
+		console.log("Fuel level in " + chevy.make + " " + chevy.model + " is: " + chevy.fuel + " gallons.");
+		chevy.stop();
+    }
+}
 
 
-// "for in" loop iterates through an object's properties
+
 /*
+// "for in" loop iterates through an object's properties
 for (var prop in chevy) {
 	console.log(prop + ": " + chevy[prop]);
 }
